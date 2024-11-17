7920c2b7640335d24a9d09ccc1a330afd5286b65
Selfrules <mattia@selfrules.org>
2024-11-17 19:33:38 +0100

    add to do list

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static('public'));
+app.use('/images', express.static(path.join(__dirname, 'public/images')));
 
 // Routes
 app.use('/api', optimizerRoutes);
